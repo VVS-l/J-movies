00f97c10aebe7ed9e4ae227f1e8e9f9b17859282
VVS-l <97415302+VVS-l@users.noreply.github.com>
2023-10-15 17:34:37 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movie.jahquazeedwards1996.workers.dev",
+  VITE_TMDB_READ_API_KEY: "b9d9d3533da8ceee0d069cc7d1e91180"
+};
