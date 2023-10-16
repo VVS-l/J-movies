ec245db29a1f3fa34a8ba7a44af440ed03b285d4
VVS-l <97415302+VVS-l@users.noreply.github.com>
2023-10-15 22:36:43 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "https://movie.jahquazeedwards1996.workers.dev",
+  VITE_CORS_PROXY_URL: "https://www.j-movies.workers.dev",
   VITE_TMDB_READ_API_KEY: "b9d9d3533da8ceee0d069cc7d1e91180"
 };
